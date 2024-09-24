f48deed4d3f83f4dac0a637b93b0bdcf29cc8bb0
voya <101844118+Voya1009@users.noreply.github.com>
2024-09-24 11:54:21 -0300

    Update Test03_BuscarJugadorTest.java

--- src/test/java/sistema/Test03_BuscarJugadorTest.java
+++ src/test/java/sistema/Test03_BuscarJugadorTest.java
@@ -6,41 +6,41 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Test03_BuscarJugadorTest {
-	Retorno retorno;
+  Retorno retorno;
 
  	@Test
 	void buscarJugadorError01(){
-    	Sistema sistema = new ImplementacionSistema();
-    	sistema.inicializarSistema(10);
+   	Sistema sistema = new ImplementacionSistema();
+   	sistema.inicializarSistema(10);
 
-    	retorno = sistema.buscarJugador("");      
-    	assertEquals(Retorno.Resultado.ERROR_1, retorno.getResultado());    
+   	retorno = sistema.buscarJugador("");      
+   	assertEquals(Retorno.Resultado.ERROR_1, retorno.getResultado());    
 
-    	retorno = sistema.buscarJugador();      
-    	assertEquals(Retorno.Resultado.ERROR_1, retorno.getResultado());    
-  	}
+   	retorno = sistema.buscarJugador();      
+   	assertEquals(Retorno.Resultado.ERROR_1, retorno.getResultado());    
+ 	}
 
-  	@Test
-  	void buscarJugadorError02(){
-    	Sistema sistema = new ImplementacionSistema();
-    	sistema.inicializarSistema(10);
+  @Test
+  void buscarJugadorError02(){
+    Sistema sistema = new ImplementacionSistema();
+   	sistema.inicializarSistema(10);
 
-    	retorno = sistema.registrarJugador("Zeka","Kim ", cat.PROFESIONAL); 
-    	assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+   	retorno = sistema.registrarJugador("Zeka","Kim ", cat.PROFESIONAL); 
+   	assertEquals(Retorno.Resultado.OK, retorno.getResultado());
 
-    	retorno = sistema.buscarJugador("Jankos");      
-    	assertEquals(Retorno.Resultado.ERROR_2, retorno.getResultado());
-  	} 
+   	retorno = sistema.buscarJugador("Jankos");      
+   	assertEquals(Retorno.Resultado.ERROR_2, retorno.getResultado());
+ 	} 
 
  	@Test
-  	void buscarJugadorOK(){
-    	Sistema sistema = new ImplementacionSistema();
-    	sistema.inicializarSistema(10);
+  void buscarJugadorOK(){
+    Sistema sistema = new ImplementacionSistema();
+    sistema.inicializarSistema(10);
 
-    	retorno = sistema.registrarJugador("Mikyx","Mihael", cat.PROFESIONAL); 
-    	assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+   	retorno = sistema.registrarJugador("Mikyx","Mihael", cat.PROFESIONAL); 
+   	assertEquals(Retorno.Resultado.OK, retorno.getResultado());
 
-    	retorno = sistema.buscarJugador("Mikyx");      
-        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
-  	}  
+   	retorno = sistema.buscarJugador("Mikyx");      
+    assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+  }  
 }
