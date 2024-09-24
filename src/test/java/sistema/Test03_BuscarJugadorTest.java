e98a6ae5eb63f3ce92ab9d0f3e497d062f830854
voya <101844118+Voya1009@users.noreply.github.com>
2024-09-24 11:51:54 -0300

    Update Test03_BuscarJugadorTest.java

--- src/test/java/sistema/Test03_BuscarJugadorTest.java
+++ src/test/java/sistema/Test03_BuscarJugadorTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Test03_BuscarJugadorTest {
 	Retorno retorno;
 
-  	@Test
+ 	@Test
 	void buscarJugadorError01(){
     	Sistema sistema = new ImplementacionSistema();
     	sistema.inicializarSistema(10);
